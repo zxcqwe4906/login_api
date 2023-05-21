020af6af48011d3975d0902c369e95dba4d049e9
Jack <zxcqwe490649064906@gmail.com>
2023-05-21 16:01:49 +0800

    Add simple signup/ POST api
    TODO: implement api content.

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 from fastapi import FastAPI
 from database.connection import Session
+from pydantic import EmailStr
 
 app = FastAPI()
+
+@app.post("/signup/")
+def signup(
+    username: str,
+    email: EmailStr,
+    password: str
+):
+    return {
+        'username': username,
+        'email': email,
+    }
